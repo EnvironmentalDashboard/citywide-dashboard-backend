7f12c42569c5ce8d806bd2714d66f614dd77d351
Sam Craig <craigsamm@gmail.com>
2019-12-24 15:50:57 -0500

    Less ugly db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 const mongo = require('mongodb').MongoClient;
 const url = process.env.DATABASE_URL || 'mongodb://cwd-mongo:27017/cleveland-cwd';
 
-mongo.connect(url, { useNewParser: true }, (err, database) => { module.exports.collection = database.db().collection('activeGlyphs') });
+mongo.connect(url, { useNewParser: true }, (err, database) => {
+  module.exports.collection = database.db().collection('activeGlyphs')
+});
 
 module.exports = { collection: null };
