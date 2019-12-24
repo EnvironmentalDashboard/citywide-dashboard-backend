235737da4f1c758d9bb3d42a27de889c724d33ad
Sam Craig <craigsamm@gmail.com>
2019-12-24 15:49:20 -0500

    Make the asynchronous database provider make more sense, though still just weird

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongo = require('mongodb').MongoClient;
 const url = process.env.DATABASE_URL || 'mongodb://cwd-mongo:27017/cleveland-cwd';
 
-mongo.connect(url, { useNewParser: true }, (err, database) => { module.exports = database });
+mongo.connect(url, { useNewParser: true }, (err, database) => { module.exports.collection = database.db().collection('activeGlyphs') });
 
-module.exports = null;
+module.exports = { collection: null };
