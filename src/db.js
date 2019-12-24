71e3172fd2ffe64baeaae5468ed939b4d8f03ece
Sam Craig <craigsamm@gmail.com>
2019-12-24 15:27:37 -0500

    Fix naming error

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongo = require('mongodb').MongoClient;
 const url = process.env.DATABASE_URL || 'mongodb://cwd-mongo:27017/cleveland-cwd';
 
-MongoClient.connect(url, { useNewParser: true }, (err, database) => { module.exports = database });
+mongo.connect(url, { useNewParser: true }, (err, database) => { module.exports = database });
 
 module.exports = null;
