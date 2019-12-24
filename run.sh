327928e57ca9a7c5f34ec5bf3957401dda3a3fbb
Sam Craig <craigsamm@gmail.com>
2019-12-24 15:46:14 -0500

    Link the database volume correctly

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -dit -p 3006:80 -v $(pwd):/usr/src/app --restart always --name cwd-be citywide-dashboard-backend
+docker run -dit -p 3006:80 -v $(pwd):/usr/src/app --restart always --link cwd-mongo:mongo --name cwd-be citywide-dashboard-backend
