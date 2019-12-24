d28d559d1c5525156f0e3d83fc68df57cf8d4e2f
Sam Craig <craigsamm@gmail.com>
2019-12-24 16:26:11 -0500

    Add a find one glyph path
    This finding one will then be abstracted to be used in finding the 
    relevant object for data caching

--- src/glyphs.js
+++ src/glyphs.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const ObjectId = require('mongodb').ObjectId;
+
 const db = require('./db');
 
-router.get('/', (req, res) => {
-  return db.collection.find({}).sort({ layer: 1 }).toArray().then(result => (
+router.get('/', (req, res) => (
+  db.collection.find({}).sort({ layer: 1 }).toArray().then(result => (
     res.json(result)
+  ))
+));
+
+router.get('/:_id', (req, res) => {
+  db.collection.findOne({
+    _id: req.params._id
+  }).then(result => (
+    // This ternary situation should not have to happen in an ideal world.
+    result ? res.json(result) : db.collection.findOne({
+      _id: new ObjectId(req.params._id)
+    }).then(oresult => (
+      res.json(oresult)
+    ))
   ))
 });
 
