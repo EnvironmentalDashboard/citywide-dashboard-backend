f8bb17dcfd348278696ee5c6979278d83056f1c9
Sam Craig <craigsamm@gmail.com>
2019-12-27 20:09:39 -0500

    Split getting a glyph by ID into its own subroutine

--- src/glyphs.js
+++ src/glyphs.js
@@ -5,23 +5,30 @@ const ObjectId = require('mongodb').ObjectId;
 
 const db = require('./db');
 
+const getGlyphById = id => (
+  db.collection.findOne({
+    _id: id
+  }).then(result => (
+    // This ternary situation should not have to happen in an ideal world.
+    result ? result : db.collection.findOne({
+      _id: new ObjectId(id)
+    }).then(oresult => (
+      oresult
+    ))
+  ))
+);
+
 router.get('/', (req, res) => (
   db.collection.find({}).sort({ layer: 1 }).toArray().then(result => (
     res.json(result)
   ))
 ));
 
-router.get('/:_id', (req, res) => {
-  db.collection.findOne({
-    _id: req.params._id
-  }).then(result => (
-    // This ternary situation should not have to happen in an ideal world.
-    result ? res.json(result) : db.collection.findOne({
-      _id: new ObjectId(req.params._id)
-    }).then(oresult => (
-      res.json(oresult)
-    ))
+router.get('/:_id', (req, res) => (
+  getGlyphById(req.params._id)
+  .then(result => (
+    res.json(result)
   ))
-});
+));
 
 module.exports = router;
