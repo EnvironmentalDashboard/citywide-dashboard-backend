963e362896bea75e24af092d91fe179c295b3384
Sam Craig <craigsamm@gmail.com>
2020-02-20 19:05:52 -0500

    Load script and run it if it's good to go

--- migrate/migrate.js
+++ migrate/migrate.js
@@ -4,15 +4,33 @@ const yargs = require('yargs');
 
 const db = require('./db');
 
-const argv = yargs.option('url',
-  {
-    description: 'the URL of the database',
+const argv = yargs.options({
+  'url': {
+    describe: 'the URL of the database',
     alias: 'u',
     type: 'string'
-  }).argv;
+  },
+  'script': {
+    describe: 'the script to run',
+    alias: 's',
+    type: 'script'
+  }
+}).argv;
 
 const url = argv.url || 'mongodb://localhost:52000/cleveland-cwd';
+const script = argv.script;
+
+if (!script) {
+  console.error('Must specify a script to run. Exiting');
+  process.exit(1);
+}
+
+const program = require(`./scripts/${script}.js`);
 
 db.connect(url).then((collection) => {
-  console.log(collection);
+  if (program.canRun(collection)) {
+    program.run(collection);
+  } else {
+    console.log('The system is already set up to handle this update.');
+  }
 });
