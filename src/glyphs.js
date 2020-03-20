a8429f98833badad97c7cb8431318f481cae85e3
acrap5 <doublebass49@gmail.com>
2020-03-20 14:18:08 -0400

    Trying to create a post route for updating messages.

--- src/glyphs.js
+++ src/glyphs.js
@@ -25,6 +25,13 @@ const getGlyphGaugesById = id => (
   ))
 );
 
+const getGlyphMessagesById = id => (
+  getGlyphById(id)
+  .then(result => (
+    result.view ? result.view.messages : []
+  ))
+);
+
 /**
  * Gets a glyph gauge by both a glyph ID and a gauge index.
  */
@@ -85,6 +92,13 @@ router.get('/:_id/gauges', (req, res) => (
   ))
 ));
 
+router.get('/:_id/messages', (req, res) => (
+  getGlyphMessagesById(req.params._id)
+  .then(result => (
+    res.json(result)
+  ))
+));
+
 /**
  * This also assumes that any view will have gauges attached.
  */
@@ -133,6 +147,27 @@ router.post('/:_id/gauges/:index/cache', (req, res) => {
       {
         $set: {
           [`view.gauges.${req.params.index - 1}.data`]: processed.parsed
+        }
+      }
+    )
+    .then(result => res.json(result));
+  }
+});
+
+router.post('/:_id/gauges/:index/messages/:num', (req, res) => {
+  if (processed.errors.length > 0) {
+    res.json({
+      'errors': processed.errors
+    });
+  } else {
+    db.collection.updateOne(
+      {
+        _id: req.params._id,
+        [`view.gauges[].${req.params.index - 1}.messages.${req.params.num}`]: { $exists: true }
+      },
+      {
+        $set: {
+          [`view.gauges[].${req.params.index - 1}.messages.${req.params.num}.text`]: res
         }
       }
     )
