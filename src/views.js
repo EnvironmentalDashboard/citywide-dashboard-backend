5c9488fe1a8fa9b385d3d6e2e13ae672fd781d2e
acrap5 <35173755+acrap5@users.noreply.github.com>
2020-04-16 17:24:04 -0500

    Create views.js

---
+++ src/views.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('./db');
+
+const getViews = () => (
+  db.collection.findOne({
+    view: {$exists: true}
+  })
+);
+
+router.get('/views', (req, res) => (
+  getViews()
+  .then(result => (
+    res.json(result)
+  ))
+));
+
+module.exports = router;
