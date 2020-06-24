6aa73eeda81573a82801c99ac82567e206f5b356
Adar Tulloch <atulloch@oberlin.edu>
2020-06-24 10:46:51 -0400

    removed accidental glyph.js modification

--- src/glyphs.js
+++ src/glyphs.js
@@ -271,40 +271,4 @@ router.post('/:_id/messages/:num', (req, res) => {
     updateMessages(req.params._id, `view.messages.${req.params.num - 1}`, processed).then(result => res.json(result));
   }
 });
-
-//Used to delete a message attached to a gauge
-/*router.delete('/:_id/deleteMessageFromGauge', req, res) => {
-  const proccessed = processMessageRequest(req);
-
-  if (process.errors.length > 0) {
-    res.json({
-      'errors' : processed.errors
-    });
-  } else {
-   try {
-     db.collection.deleteOne(
-       { "_id" : new ObjectID(req.params_id),
-     );
-   }
-  }
-}*/
-
-//Used to delete a message attachd to a view
-router.delete('/:_id/deleteMessageFromView', (req, res) => {
-    //process the incoming request
-    const processed = processMessageRequest(req);
-    if (processed.errors.length > 0) {
-      res.json({
-        'errors' : processed.errors
-      });
-    }
-    try {
-      db.collection.deleteOne(
-       { "_id" : new ObjectId(req.params_id) }
-      );
-      } catch (e) {
-            print (e);
-      }
-  });
-
 module.exports = router;
