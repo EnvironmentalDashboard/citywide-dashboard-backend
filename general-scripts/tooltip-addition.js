166ddb338bf00328077d29e23ef8b8b2b5995c6d
adar tulloch <atulloch@oberlin.edu>
2020-07-14 14:04:25 -0400

    single update call

--- general-scripts/tooltip-addition.js
+++ general-scripts/tooltip-addition.js
@@ -1,19 +1,21 @@
-const db  = require('../db.js')
-
-const ObjectID = require('mongodb').ObjectID
-const ID_String = "5d3f2b8672a1dcb5f8566562" //ID of corresponding entry.
+const db  = require('../migrate/db.js')
+const ObjectId = require('mongodb').ObjectID
+const ID_STRING = "5d3f2b8672a1dcb5f8566562" //ID of corresponding entry.
+const insertObject =  {"header" : "Water Treatment Plant", "text" : "Water from lakes, rivers, aquifers and even the ocean is collected and treated through a variety of processes to achieve needed water qualities for end uses such as drinking, irrigation, or industrial use.", "links" : []}
 
 //Make the call directly to the database
-const createToolTip = id => {
+const createToolTip = () => {
   return db.collection.update(
-    { _id: new ObjectId(id) },
-    { $set:
-       { props.tooltip :
-          { "header" : "Water Treatment Plant" },
-          { "text" : "Water from lakes, rivers, aquifers and even the ocean is collected and treated through a variety of processes to achieve needed water qualities for end uses such as drinking, irrigation, or industrial use." },
-          { "links" : [] }
-        },
-        { props.clickEffect : "showTooltip" }
+    { _id: new ObjectId(ID_STRING) },
+    { $set: {
+      [`props.tooltip`] : { "header" : "Water Treatment Plant",
+                            "text" : "Water from lakes, rivers, aquifers and even the ocean is collected and treated through a variety of processes to achieve needed water qualities for end uses such as drinking, irrigation, or industrial use.",
+                            "links" : []
+                          },
+      [`props.clickEffect`] : "showToolTip"
+      }
     }
   );
 }
+
+createToolTip();
