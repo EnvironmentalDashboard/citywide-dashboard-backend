5a7f76cdb470ce35dd135327e606557b0f163255
adar tulloch <atulloch@oberlin.edu>
2020-07-15 10:17:13 -0400

    changed format to connect to URL

--- general-scripts/tooltip-addition.js
+++ general-scripts/tooltip-addition.js
@@ -1,21 +1,39 @@
 const db  = require('../migrate/db.js')
 const ObjectId = require('mongodb').ObjectID
 const ID_STRING = "5d3f2b8672a1dcb5f8566562" //ID of corresponding entry.
-const insertObject =  {"header" : "Water Treatment Plant", "text" : "Water from lakes, rivers, aquifers and even the ocean is collected and treated through a variety of processes to achieve needed water qualities for end uses such as drinking, irrigation, or industrial use.", "links" : []}
+const URL = 'mongodb://localhost:52000/cleveland-cwd'
 
-//Make the call directly to the database
-const createToolTip = () => {
-  return db.collection.update(
-    { _id: new ObjectId(ID_STRING) },
-    { $set: {
-      [`props.tooltip`] : { "header" : "Water Treatment Plant",
-                            "text" : "Water from lakes, rivers, aquifers and even the ocean is collected and treated through a variety of processes to achieve needed water qualities for end uses such as drinking, irrigation, or industrial use.",
-                            "links" : []
-                          },
-      [`props.clickEffect`] : "showToolTip"
-      }
-    }
-  );
+const canRun = collection => {
+  new Promise ((resolve, reject) => {
+    collection.find({
+      "_id" : ObjectId(ID_STRING)
+    })
+    .toArray()
+    .then(r => {
+      resolve(r.some(i => i._id))
+    })
+  })
 }
 
-createToolTip();
+const run = collection => {
+  collection.find({
+    "_id" : ObjectId(ID_STRING)
+  })
+  .toArray()
+  .then(idEntires => {
+    idEntries.forEach(idEntry => {
+      collection.updateOne(
+        { _id: new ObjectId(ID_STRING) },
+        { $set: {
+          "props.tooltip" : { "header" : "Water Treatment Plant",
+                                "text" : "Water from lakes, rivers, aquifers and even the ocean is collected and treated through a variety of processes to achieve needed water qualities for end uses such as drinking, irrigation, or industrial use.",
+                                "links" : []
+                              },
+          "props.clickEffect" : "showToolTip"
+          }
+        }
+      )
+    })
+  })
+}
+module.exports { canRun, run };
