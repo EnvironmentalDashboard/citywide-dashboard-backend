b0c38600eb3addd713df6c3856d071083064b019
Sam Craig <craigsamm@gmail.com>
2020-07-30 09:55:47 -0400

    Add script to add the `data_url` field to relevant glyphs

---
+++ migrate/scripts/electron-data.js
@@ -0,0 +1,40 @@
+const ObjectId = require('mongodb').ObjectID
+
+const canRun = collection => (
+  new Promise ((resolve, reject) => {
+    collection.find({
+      name: 'powerline'
+    })
+    .toArray()
+    .then(r => {
+      resolve(r.length > 0)
+    });
+  })
+)
+
+const run = collection => {
+  collection.find({
+    name: 'powerline'
+  })
+  .toArray()
+  .then(idEntries => {
+    idEntries.forEach(idEntry => {
+      collection.updateOne(
+        { _id: idEntry._id },
+        {
+          $set: {
+            data_url: "https://qa.communityhub.cloud/data-hub/meters/compare/1414c3a0-cda7-11e8-9c46-02420aff0702/1414c3a0-cda7-11e8-9c46-02420aff0702"
+          }
+        }
+      )
+      .then(wc => {
+        if (!wc.writeConcernError) {
+          console.log(`Successfully updated ${idEntry._id}`);
+        } else {
+          console.error(`Error updating ${idEntry._id}`);
+        }
+      })
+    })
+  })
+}
+module.exports = { canRun, run };
